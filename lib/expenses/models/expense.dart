90361b125163be1a161ea8d123f3c16e3dc790e4
Lucas Tassi Souza <lucastassisouza@gmail.com>
2023-06-24 02:37:25 -0300

    feat: mock data

--- lib/expenses/models/expense.dart
+++ lib/expenses/models/expense.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Expense {
   String name;
   double value;
@@ -25,4 +27,20 @@ class Expense {
       category: values[3],
     );
   }
+}
+
+List<Expense> generateMockExpenses() {
+  List<Expense> expenses = [];
+
+  for (int i = 0; i < 6; i++) {
+    Expense expense = Expense(
+      name: 'Expense ${i + 1}',
+      value: Random().nextDouble() * 1000,
+      date: DateTime.now().subtract(Duration(days: i)).toString(),
+      category: 'Category ${i + 1}',
+    );
+    expenses.add(expense);
+  }
+
+  return expenses;
 }
--- lib/incomes/models/income.dart
+++ lib/incomes/models/income.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Income {
   String name;
   double value;
@@ -25,4 +27,20 @@ class Income {
       category: values[3],
     );
   }
+}
+
+List<Income> generateMockIncomes() {
+  List<Income> incomes = [];
+
+  for (int i = 0; i < 6; i++) {
+    Income income = Income(
+      name: 'Income ${i + 1}',
+      value: Random().nextDouble() * 1000,
+      date: DateTime.now().subtract(Duration(days: i)).toString(),
+      category: 'Category ${i + 1}',
+    );
+    incomes.add(income);
+  }
+
+  return incomes;
 }
